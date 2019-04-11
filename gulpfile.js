4d78f668ddd8741d08f04ab96f45c17f63633900
marcel <marcel_hoekstr@hotmail.com>
2019-04-11 10:28:29 +0200

    chnaged build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 // credits for build-script to benbaran https://github.com/benbaran/ 
-
 var
     bump = require('gulp-bump'),
     del = require('del'),
@@ -19,62 +18,21 @@ gulp.task('compile', function (cb) {
     });
 });
 
-gulp.task('bump', function () {
-    gulp.src('./package.json')
-        .pipe(bump({
-            type: 'patch'
-        }))
-        .pipe(gulp.dest('./'));
-});
-
 gulp.task('copy', function (cb) {
-
     const pkgjson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-
+    const readMe = fs.readFileSync('./README.md', 'utf8');
     delete pkgjson.scripts;
-
     delete pkgjson.devDependencies;
-
+    const filepathReadme = './dist/README.md';
     const filepath = './dist/package.json';
-
+    fs.writeFileSync(filepathReadme, readMe, 'utf-8');
     fs.writeFileSync(filepath, JSON.stringify(pkgjson, null, 2), 'utf-8');
 });
 
-gulp.task('git-add', function (cb) {
-    exec('git add -A', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-});
-
-
-gulp.task('git-commit', function (cb) {
-
-    var package = require('./package.json');
-
-    exec('git commit -m "Version ' + package.version + ' release."', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-});
-
-gulp.task('git-push', function (cb) {
-
-    exec('git push', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-});
-
 gulp.task('publish', function (cb) {
-
     exec('npm publish ./dist', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
 });
-
